b611cb11c5e09e528d6b729925336ca8f4911bb8
Daniel Magalhães <daniel.t.magalhaes@gmail.com>
2025-10-18 19:13:35 -0400

    Desafio Java

---
+++ desafio/src/Desafio.java
@@ -0,0 +1,28 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Daniel Magalhães";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("");
+        System.out.println("********************************************");
+        System.out.println("Dados Iniciais do Cliente:");
+        System.out.println("");
+        System.out.println("\nNome do Cliente: " + nome);
+        System.out.println("Tipo de Conta: " + tipoConta);
+        System.out.println("Saldo Inicial: " + saldo);
+        System.out.println("\n********************************************");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Operações");
+        System.out.println("");
+        System.out.println("1 - Consultar Saldos");
+        System.out.println("2 - Recebar Valor");
+        System.out.println("3 - Transferir Valor");
+        System.out.println("4 - Sair");
+
+
+
+    }
+}
+
