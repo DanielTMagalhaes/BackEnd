9de5994debd6ab9ec5350fe8593b99b8ae7b305c
Daniel Magalhães <daniel.t.magalhaes@gmail.com>
2025-10-19 16:26:52 -0400

    Atualizado

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,11 +1,46 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("olá, Mundo Java!");
+        Pessoa pessoa = new Pessoa();
+        pessoa.saudacao();
+
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.dobrarNumero(5);
+        System.out.println("O dobro deste Número: " + resultado);
+
+        Musica minhaMusica = new Musica();
+
+        minhaMusica.titulo = "Nome da Música";
+        minhaMusica.artista = "Nome do Artista";
+        minhaMusica.anoLancamento = 2023;
+
+        minhaMusica.exibeFichaTecnica();
+
+        minhaMusica.avalia(4.5);
+        minhaMusica.avalia(3.8);
+        minhaMusica.avalia(5.0);
+
+        double mediaAvaliacoes = minhaMusica.pegaMedia();
+        System.out.println("Média das avaliações: " + mediaAvaliacoes);
 
 
-        for (int i = 1; i <= 5 ; i++) {
-            System.out.println("i = " + i);
+        Carro meuCarro = new Carro();
+        meuCarro.modelo = "Gol";
+        meuCarro.ano = 2023;
+        meuCarro.cor = "Preto";
 
-        }
+        // Exibir ficha técnica
+        meuCarro.exibeFichaTecnica();
+
+        // Calcular idade do carro
+        System.out.println("Idade do carro: " + meuCarro.calculaIdade() + " anos");
+
+
+
+        Aluno meuAluno = new Aluno();
+        meuAluno.nome = "Akemi";
+        meuAluno.idade = 18;
+
+        meuAluno.exibeInformacoes();
+
     }
 }
